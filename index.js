91311045e84262a5f2a1e86e49146cb747a83104
quocdatx3 <84760006+quocdatx3@users.noreply.github.com>
2021-05-25 13:39:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    fs = require('fs'),
+    app = express();
+	
+const port = process.env.PORT || 8000;
+	
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/index.html');
+});
+
+app.get('/stream1', (req, res) => {
+    const file = __dirname + '/public/audio/Anh-La-Cua-Em-Karik.mp3';
+    const stat = fs.statSync(file);
+    const total = stat.size;
+    if (req.headers.range) {
+
+    }
+    fs.exists(file, (exists) => {
+        if (exists) {
+            const range = req.headers.range;
+            const parts = range.replace(/bytes=/, '').split('-');
+            const partialStart = parts[0];
+            const partialEnd = parts[1];
+
+            const start = parseInt(partialStart, 10);
+            const end = partialEnd ? parseInt(partialEnd, 10) : total - 1;
+            const chunksize = (end - start) + 1;
+            const rstream = fs.createReadStream(file, {start: start, end: end});
+			
+			
+            res.writeHead(206, {
+                'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+                'Accept-Ranges': 'bytes', 'Content-Length': chunksize,
+                'Content-Type': 'audio/mpeg'
+            });
+            rstream.pipe(res);
+
+        } else {
+            res.send('Error - 404');
+            res.end();
+            // res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
+            // fs.createReadStream(path).pipe(res);
+        }
+    });
+});
+
+app.get('/download', (req, res) => {
+    const file = __dirname + '/public/audio/Anh-La-Cua-Em-Karik.mp3';
+    res.download(file);
+});
+
+app.get('/stream2', (req, res) => {
+    const file = __dirname + '/public/audio/Minh-Cuoi-Nhau-Di-Huynh-James-Pjnboys.mp3';
+    const stat = fs.statSync(file);
+    const total = stat.size;
+    if (req.headers.range) {
+
+    }
+    fs.exists(file, (exists) => {
+        if (exists) {
+            const range = req.headers.range;
+            const parts = range.replace(/bytes=/, '').split('-');
+            const partialStart = parts[0];
+            const partialEnd = parts[1];
+
+            const start = parseInt(partialStart, 10);
+            const end = partialEnd ? parseInt(partialEnd, 10) : total - 1;
+            const chunksize = (end - start) + 1;
+            const rstream = fs.createReadStream(file, {start: start, end: end});
+			
+			
+            res.writeHead(206, {
+                'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+                'Accept-Ranges': 'bytes', 'Content-Length': chunksize,
+                'Content-Type': 'audio/mpeg'
+            });
+            rstream.pipe(res);
+
+        } else {
+            res.send('Error - 404');
+            res.end();
+            // res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
+            // fs.createReadStream(path).pipe(res);
+        }
+    });
+});
+
+app.get('/download', (req, res) => {
+    const file = __dirname + '/Data/Audio/Minh-Cuoi-Nhau-Di-Huynh-James-Pjnboys.mp3';
+    res.download(file);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
